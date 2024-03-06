fa71ce4db3179e5ed9a2c30152ba680721c147a3
Divyendu Sathe <divyendu.sathe@oracle.com>
2024-03-06 15:36:37 +0530

    Initial Commit for OCI function

---
+++ concat-java/src/main/java/com/demo/fn/ConcatNames.java
@@ -0,0 +1,12 @@
+package com.demo.fn;
+
+public class ConcatNames {
+
+    public String concatTwoNames(String input1, String input2) {
+        String lname = input1 + input2;
+
+        System.out.println("Inside Java ConcatNames function Edited now by DS"); 
+        return lname;
+    }
+
+}
